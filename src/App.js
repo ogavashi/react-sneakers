41414d1b68c2b125d48cc006d9788b16e4b5943c
oleg.gavashi@yahoo.com <oleg.gavashi@yahoo.com>
2022-05-09 14:27:39 +0300

    Added cart design, added favourite buttons to cards, added search bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,66 @@
 function App() {
   return (
     <div className="wrapper clear">
+      <div className="overlay">
+        <div className="drawer">
+          <h2 className="d-flex justify-between mb-30">
+            Cart <img className="cu-p removeBtn" src="img/btn-remove.svg" alt="Remove" />
+          </h2>
+          <div className="items">
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
+                className="cartItemImg"
+              ></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Nike Blazzer Mid Suede</p>
+                <b>2999 грн.</b>
+              </div>
+              <img className="removeBtn" src="img/btn-remove.svg" alt="Remove" />
+            </div>
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: "url(/img/sneakers/2.jpg)" }}
+                className="cartItemImg"
+              ></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Nike Blazzer Mid Suede</p>
+                <b>2999 грн.</b>
+              </div>
+              <img className="removeBtn" src="img/btn-remove.svg" alt="Remove" />
+            </div>
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: "url(/img/sneakers/3.jpg)" }}
+                className="cartItemImg"
+              ></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Nike Blazzer Mid Suede</p>
+                <b>2999 грн.</b>
+              </div>
+              <img className="removeBtn" src="img/btn-remove.svg" alt="Remove" />
+            </div>
+          </div>
+          <div className="cartTotalBlock">
+            <ul>
+              <li>
+                <span>Total: </span>
+                <div></div>
+                <b> 9000 грн. </b>
+              </li>
+              <li>
+                <span>Tax 5%:</span>
+                <div></div>
+                <b>450 грн.</b>
+              </li>
+            </ul>
+            <button className="greenButton">
+              Confirm your order <img src="img/arrow.svg" alt="Arrow" />
+            </button>
+          </div>
+        </div>
+      </div>
+
       <header className="p-40 d-flex justify-between align-center">
         <div className="d-flex align-center">
           <img className="mr-15" width={40} height={40} src="/img/logo.png" alt="Logo" />
@@ -20,9 +80,18 @@ function App() {
         </ul>
       </header>
       <div className="content p-40">
-        <h1 className="mb-40">All sneakers</h1>
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>All sneakers</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Search..." />
+          </div>
+        </div>
         <div className="sneakers d-flex">
           <div className="card">
+            <div className="favourite">
+              <img src="/img/unliked.svg" alt="Unliked" />
+            </div>
             <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers" />
             <h5>Nike Blazzer Mid Suede</h5>
             <div className="d-flex justify-between align-center">
